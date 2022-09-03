d6103e24f8a2531025582e723714fcd10bf5548b
MaxiGuiv <112709595+MaxiGuiv@users.noreply.github.com>
2022-09-03 14:07:24 +0800

    Update conversion.py

--- conversion.py
+++ conversion.py
@@ -1,19 +1,52 @@
 # Write the inputs in the next three lines. Labels must be in plural form.
-# Currently supports kilograms and grams
-from_quantity =
-from_label =
-to_label =
+#Accepted labels atm: Weight: grams,kilograms,tonnes
+from_quantity = 1050
+from_label = "grams"
+to_label = "tonnes"
 
 fq = from_quantity
 fl = from_label
 tl = to_label
 rv = 0
 
-if fl == "grams" and tl == "kilograms":
-    rv = fq / 1000
+def result():
+    print(f"{fq}{op}")
     print(f"{rv} {tl}")
-elif fl == "kilograms" and tl == "grams":
-    rv = fq * 1000
-    print(f"{rv} {tl}")
-else:
-    print("Label is either misspelled, not in plural form, or currently not supported by the program.")
+def error():
+    print("Labels are either misspelled, not in plural form, or currently not in the program.")
+
+if fl == "grams":
+    if tl == "kilograms":
+        rv = fq / 1000
+        op = "/1,000"
+        result()
+    elif tl == "tonnes":
+        rv = fq / 1000000
+        op = "/1,000,000"
+        result()
+    else:
+        error()
+
+elif fl == "kilograms":
+    if tl == "grams":
+        rv = fq * 1000
+        op = "*1,000"
+        result()
+    elif tl == "tonnes":
+        rv = fq / 1000
+        op = "/1,000"
+        result()
+    else:
+        error()
+
+elif fl == "tonnes":
+    if tl == "grams":
+        rv = fq * 1000000
+        op = "*1,000,000"
+        result()
+    elif tl == "kilograms":
+        rv = fq * 1000
+        op = "*1,000"
+        result()
+    else:
+        error()
