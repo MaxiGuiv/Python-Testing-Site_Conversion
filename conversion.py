42bf758bc175e16b7bbefb1b019dbf8978c3f6dc
MaxiGuiv <112709595+MaxiGuiv@users.noreply.github.com>
2022-09-09 07:36:19 +0800

    Update conversion.py

--- conversion.py
+++ conversion.py
@@ -1,4 +1,4 @@
-avail_lbls = ["grams", "kilograms", "tonnes"]
+avail_lbls = ["g", "kg", "mg"]
 print(f"Only accepts {avail_lbls} atm.")
 
 fq = int(input("Type the number: "))
@@ -6,33 +6,33 @@ fl = input("Type the label of this: ")
 tl = input("What label to convert this to?: ")
 
 def result():
-    print(f"Formula: {fq} {fl}{op}")
+    print(f"Formula: {fq} {fl} {op} {tl}")
     print(f"Answer: {rv} {tl}")
 def error():
     print("Labels are either misspelled, not in plural form, or currently not in the program.")
 
 if fl in avail_lbls and tl in avail_lbls:
-    if fl == "grams":
-        if tl == "kilograms":
+    if fl == "g":
+        if tl == "kg":
             rv = fq / 1000
             op = "/1000 kilograms"
-        elif tl == "tonnes":
-            rv = fq / 1000000
-            op = "/1000000 tonnes"
-    elif fl == "kilograms":
-        if tl == "grams":
+        elif tl == "mg":
+            rv = fq * 1000
+            op = "*1000"
+    elif fl == "kg":
+        if tl == "g":
             rv = fq * 1000
             op = "*1000 grams"
-        elif tl == "tonnes":
-            rv = fq / 1000
-            op = "/1000 tonnes"
-    elif fl == "tonnes":
-        if tl == "grams":
+        elif tl == "mg":
             rv = fq * 1000000
-            op = "*1000000 grams"
-        elif tl == "kilograms":
-            rv = fq * 1000
-            op = "*1000 kilograms"
+            op = "*1000000"
+    elif fl == "mg":
+        if tl == "g":
+            rv = fq / 1000
+            op = "/1000"
+        elif tl == "kg":
+            rv = fq / 1000000
+            op = "/1000000"
     result()
 else:
     error()
